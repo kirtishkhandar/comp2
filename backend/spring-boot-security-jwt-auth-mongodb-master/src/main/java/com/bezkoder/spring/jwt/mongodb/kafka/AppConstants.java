c62ef81cf0ac51ca8b2b86f63ce9361521ba56d9
kirtishkhandar <kirtish.khandar@gmail.com>
2022-08-18 23:51:00 +0530

    namechange

--- backend/spring-boot-security-jwt-auth-mongodb-master/src/main/java/com/bezkoder/spring/jwt/mongodb/kafka/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.bezkoder.spring.jwt.mongodb.kafka;
-
-public class AppConstants {
-	public static final String TOPIC_NAME = "javaguides";
-	public static final String GROUP_ID = "group_id";
-
-}
